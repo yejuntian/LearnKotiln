5fcefc21b23e419db81a7cc7ef1eabda95f44de7
tianyejun <tianyejun@adsconflux.com>
2024-03-23 15:30:58 +0800

    启用dataBinding、添加Retrofit 依赖

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -33,10 +33,15 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+    //添加dataBing库支持
+    buildFeatures {
+        dataBinding = true
+    }
 }
 
 dependencies {
 
+    //noinspection GradleCompatible
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
@@ -44,4 +49,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // Retrofit 核心库
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    // Retrofit Gson 转换器
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    // 如果你打算使用 Kotlin 协程
+    implementation("com.squareup.retrofit2:adapter-rxjava3:2.9.0")
 }
