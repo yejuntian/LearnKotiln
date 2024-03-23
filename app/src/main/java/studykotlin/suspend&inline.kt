73786486a737d5d62162674b0cd137c4d2d22e72
tianyejun <tianyejun@adsconflux.com>
2024-03-23 10:47:43 +0800

    suspend和inline关键词

--- app/src/main/java/studykotlin/suspend&inline.kt
+++ app/src/main/java/studykotlin/suspend&inline.kt
@@ -4,7 +4,8 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.runBlocking
 
 /**
- * suspend关键词作用：suspend修饰的函数，可以在函数中进行耗时操作。
+ * suspend关键词作用：使用suspend关键字修饰的函数叫做挂起函数。
+ * 挂起函数：只能在"协程代码块"或"其他挂起函数"中调用
  *
  * inline关键词作用：把调用inline代码的这一块，直接替换成其代码内容。
  * inline函数：适合具有函数类型的参数
@@ -12,7 +13,7 @@ import kotlinx.coroutines.runBlocking
 fun main() {
     runBlocking {
         val test = testSuspend()
-        println(test)
+        println (test)
         println("********测试inline函数*********")
         println(testInline())
     }
