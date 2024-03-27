9e2b84d649d57477a7d95ce27e2cf2c901e565d8
tianyejun <tianyejun@adsconflux.com>
2024-03-27 15:29:37 +0800

    修改统一跳转逻辑

--- app/src/main/java/com/study/learnkotiln/DebugActivity.kt
+++ app/src/main/java/com/study/learnkotiln/DebugActivity.kt
@@ -1,8 +1,14 @@
 package com.study.learnkotiln
 
+import android.app.Activity
+import android.content.Intent
 import android.content.SharedPreferences
 import android.os.Bundle
 import android.preference.PreferenceActivity
+import studykotlin.dongnao.CoroutineExceptionHandlerActivity
+import studykotlin.dongnao.MainScopeActivity
+import studykotlin.dongnao.MainScopeActivity2
+
 
 class DebugActivity : PreferenceActivity(), SharedPreferences.OnSharedPreferenceChangeListener {
 
@@ -10,6 +16,24 @@ class DebugActivity : PreferenceActivity(), SharedPreferences.OnSharedPreference
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_debug)
         addPreferencesFromResource(R.xml.debug_activitys)
+
+        setOnclickListener()
+    }
+
+    private fun setOnclickListener() {
+        setupPreferenceClickListener("MainScopeActivity", MainScopeActivity::class.java)
+        setupPreferenceClickListener("MainScopeActivity2", MainScopeActivity2::class.java)
+        setupPreferenceClickListener(
+            "CoroutineExceptionHandlerActivity",
+            CoroutineExceptionHandlerActivity::class.java
+        )
+    }
+
+    private fun <T : Activity> setupPreferenceClickListener(key: String, activityClass: Class<T>) {
+        findPreference(key)?.setOnPreferenceClickListener {
+            startActivity(Intent(this@DebugActivity, activityClass))
+            true
+        }
     }
 
     override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences?, key: String?) {
