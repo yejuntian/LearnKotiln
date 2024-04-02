d359ddaf5e4f9d686c3d82cf8ff10ec5c2241d2e
tianyejun <tianyejun@adsconflux.com>
2024-04-02 15:11:49 +0800

    Flow补充

--- app/src/main/java/studykotlin/dongnao/flow/2-Flow冷流.kt
+++ app/src/main/java/studykotlin/dongnao/flow/2-Flow冷流.kt
@@ -1,19 +1,21 @@
 package studykotlin.dongnao.flow
 
 import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
 
 /**
  * 冷流
  * Flow：是一种类似序列的冷流，flow构建器中的代码直到流收集的时候才会执行。
+ *
+ * Flow 可以实现一对多的情况，多个收集器可以同时收集同一个 Flow 发出的数据。
  * @see testFlowIsCode
  *
  */
 fun main() {
     println("********** 测试Flow为冷流，直到收集Flow构建器中代码才会执行 ************")
     testFlowIsCode()
-
 }
 
 fun testFlowIsCode() = runBlocking {
@@ -33,5 +35,23 @@ fun getSimpleFlow() = flow<Int> {
     for (i in 1..3) {
         delay(200)
         emit(i)
+    }
+}
+
+fun testOne2Many() {
+    fun main() = runBlocking {
+        val dataFlow: Flow<Int> = flow {
+            repeat(5) {
+                emit(it)
+            }
+        }
+
+        val collector1 = dataFlow.collect {
+            println("Collector 1 received: $it")
+        }
+
+        val collector2 = dataFlow.collect {
+            println("Collector 2 received: $it")
+        }
     }
 }
